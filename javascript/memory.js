1e05c54bd4aaabd7c47707c10b1d871fd449ba1e
malak-joumaa <malak.joumaa.dev@gmail.com>
2022-05-20 15:59:36 +0300

    Adding color change feature on mousepress

--- javascript/memory.js
+++ javascript/memory.js
@@ -2,3 +2,50 @@ let green = document.getElementById("square1");
 let red = document.getElementById("square2");
 let yellow = document.getElementById("square3");
 let blue = document.getElementById("square4");
+let level = document.getElementById("instruction");
+
+
+// const downR = downListener(red)
+// const upR = upListener(red)
+// const downG = downListener(green)
+// const upG = upListener(green)
+// const downB = downListener(blue)
+// const upB = upListener(blue)
+// const downY = downListener(yellow)
+// const upY = upListener(yellow)
+
+// red.addEventListener('mousedown',downR);
+// red.addEventListener('mouseup',upR);
+
+// green.addEventListener('mousedown',downG);
+// green.addEventListener('mouseup',upG);
+
+// blue.addEventListener('mousedown',downB);
+// blue.addEventListener('mouseup',upB);
+
+// yellow.addEventListener('mousedown',downY);
+// yellow.addEventListener('mouseup',upY);
+
+
+const downR = downListener(red);
+const upR = upListener(red);
+
+red.addEventListener('mousedown',downR);
+red.addEventListener('mouseup',upR);
+
+function downListener(color){
+    return function inner(event){
+        console.log(event)
+        color.style.backgroundColor= "#747474";
+        color.style.boxShadow=" 0 0 10px #747474";
+    }
+}
+
+function upListener(color){
+    return function inner(event){
+        console.log(event)
+        color.style.backgroundColor= null;
+        color.style.boxShadow= null;
+    }
+}
+
