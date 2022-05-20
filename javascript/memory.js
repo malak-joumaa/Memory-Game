e4fef60bd51e424123394566b72987f13048d4ae
malak-joumaa <malak.joumaa.dev@gmail.com>
2022-05-20 12:58:47 +0300

    Assigning variables in javascript

---
+++ javascript/memory.js
@@ -0,0 +1,4 @@
+let green = document.getElementById("square1");
+let red = document.getElementById("square2");
+let yellow = document.getElementById("square3");
+let blue = document.getElementById("square4");
