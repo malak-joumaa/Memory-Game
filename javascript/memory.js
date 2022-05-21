2987a952b6c08d55d406c415beacb89b57a34cf0
malak-joumaa <malak.joumaa.dev@gmail.com>
2022-05-21 16:03:56 +0300

    Resetting game after the player wins or loses

--- javascript/memory.js
+++ javascript/memory.js
@@ -34,8 +34,6 @@ function downListener(color){
     }
 }
 
-
-
 function upListener(color){
     return function inner(event){
         console.log(event)
@@ -44,13 +42,7 @@ function upListener(color){
     }
 }
 
-function colorChange(color){
-    color.style.backgroundColor= "#747474";
-    color.style.boxShadow=" 0 0 10px #747474";
-};
-
 let sequence=[]
-let usersurvives = false;
 
 let colors=[[downR,upR], [downG,upG], [downY,upY], [downB,upB]]
 let divs = [red,green,yellow,blue];
@@ -61,66 +53,46 @@ const delay = (ms) => {
     return new Promise(resolve => setTimeout(resolve, ms))
   }
 
-  var cont = true
 var score=0;
 var count = 0;
 var i = 0;
 var j=0;
+// START GAME
 async function startGame(){
     console.log("Here we go again");
-    // if(i>1){
-    //     col[sequence[i-1]].removeEventListener('click',eventR);
-    //     console.log(count)
-    //     }
-
     score++;
-    level.innerText = "Level "+score;
-  //  while(usersurvives){
-     //   usersurvives = false;
-     if (score<15){
+     if (score<3){
+        level.innerText = "Level "+score;
         i=0;
         await delay(1000);
         sequence.push(Math.floor(Math.random() * 4));
-        //sconsole.log(sequence)
+        
         while(i<sequence.length) {
             setTimeout(colors[sequence[i]][0],0),
             setTimeout(colors[sequence[i]][1],250)
             await delay(1000);
-            //console.log(col[sequence[0]]);
             i++;
         }
         j=0;
         count=0;
     }
     else{
-        alert("You Won!!")
+        level.innerText = "You Win! Press Any Key to Play Again";
+        score=0;
+        sequence=[];
+        document.addEventListener("keypress", startGame);
     }
     }
-    const clickR = isCorrect(red)
-    const clickG = isCorrect(green)
-    const clickB = isCorrect(blue)
-    const clickY = isCorrect(yellow)
+    const clickR = isCorrect(red);
+    const clickG = isCorrect(green);
+    const clickB = isCorrect(blue);
+    const clickY = isCorrect(yellow);
 
     red.onclick= clickR;
     green.onclick= clickG;
     yellow.onclick= clickY;
     blue.onclick= clickB;
 
-    
-
-    // function isCorrect(color){
-    //     console.log("isCorrect");
-    //     if(color===divs[sequence[j]]){
-    //         count++;
-    //         j++;
-    //     }
-    //     else if(color!==divs[sequence[j]]){
-    //         alert("You Lost!")
-    //     }
-    //     if(count==sequence.length){
-    //         startGame();
-    //     }
-    // }
     function isCorrect(color){
         return function inner(event){
             console.log("isCorrect");
@@ -130,7 +102,11 @@ async function startGame(){
             console.log("count: "+count+" j:"+j)
         }
         else if(color!==divs[sequence[j]]){
-            alert("You Lost!")
+            console.log(score);
+            level.innerText = "Game Over, Press Any Key to Restart";
+            score=0;
+            sequence=[];
+            document.addEventListener("keypress", startGame);
         }
         if(count==sequence.length){
             console.log("start success");
@@ -138,28 +114,3 @@ async function startGame(){
         }
         }
     }
-        //console.log("i after loop:"+i);
-        //console.log(col[sequence[i-2]]);
-        
-//         divs[sequence[i-1]].addEventListener('click', eventR);
-//    // }
-//    function eventR(){
-//     count++;
-//     var checker = sequence.slice(0,-1)
-//     if (checker.includes(sequence[i-1])){
-//         count--;
-//         console.log("new"+count)
-//     }
-//     console.log(i,count+"HII");
-//     if (count==sequence.length){
-//         startGame();
-//     }
-//    }
-
-
-
-// If every event listener returns true, call startGame
-
-    
-
-
