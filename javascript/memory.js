13d434ad50998bfd614923ec60d7f07c69e14661
malak-joumaa <malak.joumaa.dev@gmail.com>
2022-05-21 07:26:01 +0300

    Adding startGame function and including flash sequence

--- javascript/memory.js
+++ javascript/memory.js
@@ -5,33 +5,26 @@ let blue = document.getElementById("square4");
 let level = document.getElementById("instruction");
 
 
-// const downR = downListener(red)
-// const upR = upListener(red)
-// const downG = downListener(green)
-// const upG = upListener(green)
-// const downB = downListener(blue)
-// const upB = upListener(blue)
-// const downY = downListener(yellow)
-// const upY = upListener(yellow)
-
-// red.addEventListener('mousedown',downR);
-// red.addEventListener('mouseup',upR);
-
-// green.addEventListener('mousedown',downG);
-// green.addEventListener('mouseup',upG);
-
-// blue.addEventListener('mousedown',downB);
-// blue.addEventListener('mouseup',upB);
-
-// yellow.addEventListener('mousedown',downY);
-// yellow.addEventListener('mouseup',upY);
-
-
-const downR = downListener(red);
-const upR = upListener(red);
+const downR = downListener(red)
+const upR = upListener(red)
+const downG = downListener(green)
+const upG = upListener(green)
+const downB = downListener(blue)
+const upB = upListener(blue)
+const downY = downListener(yellow)
+const upY = upListener(yellow)
 
 red.addEventListener('mousedown',downR);
 red.addEventListener('mouseup',upR);
+
+green.addEventListener('mousedown',downG);
+green.addEventListener('mouseup',upG);
+
+blue.addEventListener('mousedown',downB);
+blue.addEventListener('mouseup',upB);
+
+yellow.addEventListener('mousedown',downY);
+yellow.addEventListener('mouseup',upY);
 
 function downListener(color){
     return function inner(event){
@@ -54,8 +47,29 @@ function colorChange(color){
     color.style.boxShadow=" 0 0 10px #747474";
 };
 
-document.addEventListener("keypress", function(event) {
-	
-    setTimeout(downR,0);
-    setTimeout(upR,250);
-});
+let sequence=[]
+let usersurvives = true
+
+let colors=[[downR,upR], [downG,upG], [downY,upY], [downB,upB]]
+
+document.addEventListener("keypress", startGame);
+
+const delay = (ms) => {
+    return new Promise(resolve => setTimeout(resolve, ms))
+  }
+
+async function startGame(){
+    while(usersurvives){
+        sequence.push(Math.floor(Math.random() * 4));
+        console.log(sequence)
+        for(var i=0; i<sequence.length; i++){
+            setTimeout(colors[sequence[i]][0],0),
+            setTimeout(colors[sequence[i]][1],250)
+            await delay(2000);
+            console.log(sequence);
+        }
+        
+        if(i==2)
+            usersurvives = false;
+    }
+}
